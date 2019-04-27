f82e23930ac150d9af3377a654054d04e151be20
Peng Cheng <pengc@microsoft.com>
2019-04-27 08:47:36 +0800

    WinDLL: Add Apps - Add access violation test cases

---
+++ apps/access_violation_case_1/hsavc1.cpp
@@ -0,0 +1,81 @@
+#include "stdio.h"
+#include "hs.h"
+
+// note this function is never called, but without it this code works fine
+static int __cdecl eventHandler(unsigned int id, unsigned long long from,
+                        unsigned long long to, unsigned int flags, void *ctx) {
+    printf("Match for pattern");
+    return 0;
+}
+
+char* crs_patterns[] = {
+  ".",
+  "dummy2",
+};
+
+static int __cdecl blowupEventHandler(unsigned int id, unsigned long long from,
+    unsigned long long to, unsigned int flags, void *ctx)
+{
+    return 1;
+}
+
+
+int __cdecl main(int argc, char* argv[])
+{
+    hs_database_t* pRegexDatabase = nullptr;
+    hs_compile_error_t *compile_err = nullptr;
+    hs_error_t hsError = hs_compile(
+        "Sample?Value",
+        0,
+        HS_MODE_BLOCK,
+        nullptr,
+        &pRegexDatabase,
+        &compile_err);
+    if (hsError != HS_SUCCESS)
+    {
+        printf("Dead 1\n");
+        exit(-2);
+    }
+
+    hs_scratch_t* pScratch = nullptr;
+    hsError = hs_alloc_scratch(pRegexDatabase, &pScratch);
+    if (hsError != HS_SUCCESS)
+    {
+        printf("Dead 2: %d\n", (int)hsError);
+        exit(-2);
+    }
+
+    bool matched = false;
+    printf("Scan 1\n");
+    hsError = hs_scan(
+        pRegexDatabase,
+        "Sample?Value",
+        12,
+        0,
+        pScratch,
+        blowupEventHandler,
+        &matched);
+    if (hsError != HS_SUCCESS)
+    {
+        printf("Dead 3\n");
+        exit(-2);
+    }
+
+    printf("Scan 2\n");
+    hsError = hs_scan(
+        pRegexDatabase,
+        "SampleeValue",
+        12,
+        0,
+        pScratch,
+        blowupEventHandler,
+        &matched);
+    if (hsError != HS_SUCCESS)
+    {
+        printf("Dead 4\n");
+        exit(-2);
+    }
+
+    printf("good exit");
+    return 0;
+}
---
+++ apps/access_violation_case_2/hsavc2.cpp
@@ -0,0 +1,111 @@
+#include "stdio.h"
+#include "hs.h"
+
+// note this function is never called, but without it this code works fine
+static int __cdecl eventHandler(unsigned int id, unsigned long long from,
+                        unsigned long long to, unsigned int flags, void *ctx) {
+    printf("Match for pattern");
+    return 0;
+}
+
+char* crs_patterns[] = {
+  ".",
+  "dummy2",
+};
+
+static int __cdecl blowupEventHandler(unsigned int id, unsigned long long from,
+    unsigned long long to, unsigned int flags, void *ctx)
+{
+    return 1;
+}
+
+
+int __cdecl main(int argc, char* argv[])
+{
+    hs_database_t* pRegexDatabase = nullptr;
+    hs_compile_error_t *compile_err = nullptr;
+    hs_error_t hsError = hs_compile(
+        "Sample?Value",
+        0,
+        HS_MODE_BLOCK,
+        nullptr,
+        &pRegexDatabase,
+        &compile_err);
+    if (hsError != HS_SUCCESS)
+    {
+        printf("Dead 1\n");
+        exit(-2);
+    }
+
+    hs_scratch_t* pScratch = nullptr;
+    hsError = hs_alloc_scratch(pRegexDatabase, &pScratch);
+    if (hsError != HS_SUCCESS)
+    {
+        printf("Dead 2\n");
+        exit(-2);
+    }
+
+    bool matched = false;
+    printf("Scan 1\n");
+    hsError = hs_scan(
+        pRegexDatabase,
+        "SampleValue",
+        11,
+        0,
+        pScratch,
+        blowupEventHandler,
+        &matched);
+    if (hsError != HS_SUCCESS && hsError != HS_SCAN_TERMINATED)
+    {
+        printf("Dead 3: %d\n", hsError);
+        exit(-2);
+    }
+
+    printf("Scan 2\n");
+    hsError = hs_scan(
+        pRegexDatabase,
+        "SamplValue",
+        10,
+        0,
+        pScratch,
+        blowupEventHandler,
+        &matched);
+    if (hsError != HS_SUCCESS && hsError != HS_SCAN_TERMINATED)
+    {
+        printf("Dead 4: %d\n", hsError);
+        exit(-2);
+    }
+
+    printf("Scan 3\n");
+    hsError = hs_scan(
+        pRegexDatabase,
+        "Sample?Value",
+        12,
+        0,
+        pScratch,
+        blowupEventHandler,
+        &matched);
+    if (hsError != HS_SUCCESS)
+    {
+        printf("Dead 5\n");
+        exit(-2);
+    }
+
+    printf("Scan 4\n");
+    hsError = hs_scan(
+        pRegexDatabase,
+        "SampleeValue",
+        12,
+        0,
+        pScratch,
+        blowupEventHandler,
+        &matched);
+    if (hsError != HS_SUCCESS)
+    {
+        printf("Dead 6\n");
+        exit(-2);
+    }
+
+    printf("good exit");
+    return 0;
+}
